a67871a0e535b542c4abf951b6c9d2e0d370fcbf
danie <danie.van.zyl@gmail.com>
2025-03-02 18:26:32 +0200

    refactor

--- cmd/main.go
+++ cmd/main.go
@@ -3,31 +3,75 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
 	"strings"
 
 	sensitivepatterns "github.com/danievanzyl/zshhistorymasker/pkg/sensitive_patterns"
 )
 
-func main() {
-	file, err := os.Open(os.Getenv("HOME") + "/.zsh_history")
+var historyFileLocation = fmt.Sprintf("%s/.zsh_history", os.Getenv("HOME"))
+
+var bakHistoryFileLocation = fmt.Sprintf("%s_bak", historyFileLocation)
+
+func backup() {
+	history, err := os.Open(historyFileLocation)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error opening file: %v\n", err)
 		os.Exit(1)
 	}
-	defer file.Close()
+	defer history.Close()
 
-	var commands []string
+	backupFile, err := os.Create(bakHistoryFileLocation)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error creating backup file: %v\n", err)
+		os.Exit(1)
+	}
+
+	defer backupFile.Close()
+
+	uu, err := io.Copy(backupFile, history)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error copying backup file: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println("written bytes", uu)
+}
+
+func main() {
+
+	fmt.Println("scanning and updating", historyFileLocation)
+	backup()
+	fmt.Println("backed up current .zsh_history (this will contain sensitive information on first run)")
+
+	// var commands []string
 	var currentCommand strings.Builder
-	scanner := bufio.NewScanner(file)
+	history, err := os.Open(bakHistoryFileLocation)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error opening file: %v\n", err)
+		os.Exit(1)
+	}
+	defer history.Close()
+
+	newHistory, err := os.Create(historyFileLocation)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error creating new history file: %v\n", err)
+		os.Exit(1)
+	}
+	defer newHistory.Close()
+
+	scanner := bufio.NewScanner(history)
 	for scanner.Scan() {
 		line := scanner.Text()
+
 		line = sensitivepatterns.MaskSensitiveInfo(line)
 
 		// If line starts with : and we have a previous command, append it
 		if strings.HasPrefix(line, ":") {
 			if currentCommand.Len() > 0 {
-				commands = append(commands, strings.TrimSpace(currentCommand.String()))
+				// commands = append(commands, strings.TrimSpace(currentCommand.String()))
+				newHistory.WriteString(fmt.Sprintf("%s\n", strings.TrimSpace(currentCommand.String())))
 				currentCommand.Reset()
 			}
 			currentCommand.WriteString(line)
@@ -42,13 +86,11 @@ func main() {
 
 	// Don't forget to append the last command
 	if currentCommand.Len() > 0 {
-		commands = append(commands, strings.TrimSpace(currentCommand.String()))
+		newHistory.WriteString(strings.TrimSpace(currentCommand.String()))
 	}
 
 	if err := scanner.Err(); err != nil {
 		panic(err)
 	}
-	for _, cmd := range commands {
-		fmt.Println("xxxx>  --- ", cmd)
-	}
+
 }
