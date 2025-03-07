3289c99a992dce8fd7fbd0aaa9449abcf7afc679
Danie van Zyl <daniev@ringier.co.za>
2025-03-07 15:54:21 +0200

    wip

--- cmd/main.go
+++ cmd/main.go
@@ -6,10 +6,11 @@ import (
 	"log"
 	"os"
 
+	"github.com/danievanzyl/zshhistorymasker/pkg/actions"
 	"github.com/urfave/cli/v3"
 )
 
-var version = ""
+var version = "v0.0.0"
 var historyFileLocation = fmt.Sprintf("%s/.zsh_history", os.Getenv("HOME"))
 
 var bakHistoryFileLocation = fmt.Sprintf("%s_bak", historyFileLocation)
@@ -17,12 +18,17 @@ var bakHistoryFileLocation = fmt.Sprintf("%s_bak", historyFileLocation)
 func main() {
 
 	cmd := &cli.Command{
-		Name:  "boom",
-		Usage: "make an explosive entrance",
-		Action: func(context.Context, *cli.Command) error {
-			fmt.Println("boom! I say!")
-			return nil
+		Version: version,
+		Name:    "boom",
+		Usage:   "make an explosive entrance",
+		Flags: []cli.Flag{
+
+			&cli.StringFlag{Name: "orig-history", Aliases: []string{"z"}, Value: historyFileLocation},
+			&cli.StringFlag{Name: "bak-history", Aliases: []string{"Z"}, Value: bakHistoryFileLocation},
 		},
+		Before: actions.Backup,
+		Action: actions.Mask,
+		After:  actions.Done,
 	}
 
 	if err := cmd.Run(context.Background(), os.Args); err != nil {
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/danievanzyl/zshhistorymasker
 go 1.23.6
 
 require github.com/urfave/cli/v3 v3.0.0-beta1
+
+replace github.com/danievanzyl/zshhistorymasker/pkg => ./pkg
